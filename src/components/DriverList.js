6c638b1b271ca99c3b5379086f9e503769a30465
Silas H. Rasch <silasrasch@gmail.com>
2024-03-08 23:42:18 +0100

    api lol

--- src/components/DriverList.js
+++ src/components/DriverList.js
@@ -40,8 +40,35 @@ const DriverList = () => {
                 </div>
             )
         })
+        // Formula1API!
+        // content = drivers.map((driver) => {
+        //     return (
+        //         <div key={driver.driverNumber} className='driver'>
+        //             <div className='d-flex driver-top'>
+        //                 <div className='column info-column'>
+        //                     <p className='driver-no'>{driver.driverNumber} <hr /></p>
+        //                     <p>{driver.championships} championships<hr /></p>
+        //                     <p>{driver.age} yrs old<hr /></p>
+        //                 </div>
+        //                 <div className='column'>
+        //                     <img className='driver-avatar' alt={driver.name + " portrait"} src={`${driver.imgPath}`} />
+        //                 </div>
+        //             </div>
+        //             <div className='d-flex driver-bottom'>
+        //                 <div className='column'>
+        //                     <p>{driver.name}</p> 
+        //                     <hr />
+        //                     <p>{driver.team}</p>
+        //                 </div>
+        //                 <div className='column'>
+        //                     <p className='pts'>{driver.points} pts</p>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     )
+        // })
     } else if (isError) {
-        content = <p>{error}</p>
+        content = <p>{error.error}</p>
     }
 
     return (
