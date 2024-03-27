a5c3fb5c03b4ce846a97d2fe34cbb876c682a72c
Silas H. Rasch <silasrasch@gmail.com>
2024-03-27 23:30:28 +0100

    new new update

--- src/routes/UpdateTeam.js
+++ src/routes/UpdateTeam.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import Team from '../components/Team';
 import Navbar from '../components/Navbar';
 import { useGetTeamByIdQuery } from '../store/api/apiSlice';
 import { NavLink } from 'react-router-dom';
+import Points from '../components/Points';
 
 const UpdateTeam = () => {
     const { id } = useParams() 
     const { data, isLoading, isSuccess, isError, error } = useGetTeamByIdQuery(id)
     
+    const team = data
+
     let content;
     if (isLoading) {
         content = <p className='loading'>Loading <i className="fa fa-circle-o-notch fa-spin" style={{fontSize: '24px'}}></i></p>
     } else if (isSuccess) {
-        // console.log(data)
-        content = <Team key={id} team={data} index={1}/>
+        const classes = "team card " + team.teamColors
+        content = (
+            <div className={classes}>
+                <div className='d-flex card-top'>
+                    <div className='column'>
+                        <div>
+                            {/* <p className='bold'>{team.name}</p> */}
+                            <input className='bold' placeholder={team.name}></input>
+                            <hr />
+                        </div>
+                        <div>
+                            {/* <p>{team.worldChampionships} championships</p> */}
+                            <p><input placeholder={team.worldChampionships}></input> championships</p>
+                            <hr/>
+                        </div>
+                        <div>
+                            <p>Team Chief</p>
+                            {/* <p>{team.teamChief}</p> */}
+                            <input placeholder={team.teamChief}></input>
+                            <hr/>
+                        </div>
+                    </div>
+                    <div className='column'>
+                        <img className='avatar' alt={team.name + " logo"} src={`${team.logoImgPath}`} />
+                    </div>
+                </div>
+                <div className='d-flex card-bottom'>
+                    <div className='column'>
+                        { team.drivers[0] !== undefined ? <p>{team.drivers[0].name}</p> : <p>Driver unavailable</p>}
+                        <hr />
+                        { team.drivers[1] !== undefined ? <p>{team.drivers[1].name}</p> : <p>Driver unavailable</p>}
+                    </div>
+                    <p className='pts team-points' style={Points(1)}><input placeholder={team.points}></input> pts</p>
+                </div>
+            </div>
+        )
     } else if (isError) {
         content = <p>{error.error}</p>
     }
